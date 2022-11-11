8844ba2c45c31c2d73015ee318015c46572ff98d
bassjoe <18555116+bassjoe@users.noreply.github.com>
2022-11-11 21:35:24 +0100

    vite pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,36 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 
 const config = {
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		SvelteKitPWA({
+			includeAssets: ['favicon.ico', 'icon-192.png', 'icon-512.png'],
+			manifest: {
+				name: 'pwa5-n',
+				short_name: 'pwa5-s',
+				description: 'pwa #5',
+				start_url: '.',
+				scope: '.',
+				display: 'standalone',
+				theme_color: '#eff',
+				background_color: '#fff',
+				icons: [
+					{
+						src: 'icon-192.png',
+						sizes: '192x192',
+						type: 'image/png',
+						purpose: 'maskable'
+					},
+					{
+						src: 'icon-512.png',
+						sizes: '512x512',
+						type: 'image/png'
+					}
+				]
+			}
+		})
+	]
 };
 
 export default config;
