644733fd1bdca1136bf69b5d15ccc7a5c0e01be0
bassjoe <18555116+bassjoe@users.noreply.github.com>
2023-04-12 19:37:56 +0000

    Deploying to gh-pages from @ bassjoe/pwa5@a6838464fc8cc830bd0887d061d745d6528e92f5 🚀

---
+++ _app/immutable/chunks/stores.03e00664.js
@@ -0,0 +1,1 @@
+import"./index.17e9b1a2.js";import{s as e}from"./singletons.ebff4d39.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
